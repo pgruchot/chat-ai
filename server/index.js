6c85a27bfdddf58136e2037d3dd762111d845150
Patryk Gruchot <patryk.gruchot@outlook.com>
2023-06-11 17:21:23 +0200

    ai chat setup

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import { Configuration, OpenAIApi } from "openai";
+import openAiRoutes from "./routes/openai.js";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -21,9 +22,12 @@ app.use(cors());
 /* OPEN AI CONFIGURATION */
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPEN_API_KEY,
 });
-const openai = new OpenAIApi(configuration);
+export const openai = new OpenAIApi(configuration);
+
+/* ROUTES */
+app.use("/openai", openAiRoutes);
 
 /* SERVER SETUP */
 const PORT = process.env.PORT || 9000;
